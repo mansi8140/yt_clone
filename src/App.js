dd5d326b4fb89d0657e753d9fd1917550eab9397
mansi8140 <mansi.study8140@gmail.com>
2023-11-07 11:45:01 -0600

    Added css for _base and _app

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import Header from './componenets/header/Header'
 import Sidebar from './componenets/sidebar/Sidebar'
 import HomeScreen from './screens/homeScreen/HomeScreen'
 import { Container } from 'react-bootstrap'
+import './_app.scss'
 const App = () => {
   return (
     <>
     <Header />
-    <div className='app-container'>
+    <div className='app-container border obrder-info'>
       <Sidebar />
       <Container fluid className='app_main'>
         <HomeScreen />
