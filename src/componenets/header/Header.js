f918fda0537f08b1f41027b19530e559067c3697
mansi8140 <mansi.study8140@gmail.com>
2023-11-07 22:36:31 -0600

    Added header content and it's css

--- src/componenets/header/Header.js
+++ src/componenets/header/Header.js
@@ -1,10 +1,33 @@
 import React from 'react'
 import './_header.scss'
-
+import {FaBars} from 'react-icons/fa'
+import { AiOutlineSearch } from 'react-icons/ai'
+import { MdNotifications, MdApps } from 'react-icons/md'
 const Header = () => {
   return (
-    <div className='border border-dark'>
-      header
+    <div className='header'>
+      <FaBars className='header_menu' size={26} />
+        <img 
+          src='http://pngimg.com/uploads/youtube/youtube_PNG2.png' 
+          className='header_logo'
+          alt= '' />
+
+        <form>
+            <input type='text' placeholder='Serach' />
+            <button type='submit'>
+              <AiOutlineSearch size={22}/>
+            </button>
+        </form>
+
+        <div className='header_icons'>
+          <MdNotifications size ={26} />
+          <MdApps size={26}/>
+          <img 
+            src='https://www.looper.com/img/gallery/the-untold-truth-of-neytiri-from-avatar/intro-1652914528.webp'
+            alt='user'
+          />
+
+        </div>
     </div>
   )
 }
