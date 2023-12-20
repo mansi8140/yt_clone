da0156b58109e9a91100b8169105088bdd952fb8
mansi8140 <mansi.study8140@gmail.com>
2023-12-20 16:07:50 -0600

    Added Firebase
    Setup to connect app with firebase

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDE0cWGdLu8gLXFEMYh6bFQBKQlbAX9gRg",
+    authDomain: "yt-bymansi.firebaseapp.com",
+    projectId: "yt-bymansi",
+    storageBucket: "yt-bymansi.appspot.com",
+    messagingSenderId: "304212504759",
+    appId: "1:304212504759:web:8bdfc5770b6f0f44d89d32"
+  };
+
+
+firebase.initializeApp(firebaseConfig)
